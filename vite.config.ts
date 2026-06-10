4001e5dd977ceda37e95034d44e009f563bfe73c
Bit Nimble <me@kumo.dev>
2026-06-10 13:52:41 +1000

    change dev port

--- vite.config.ts
+++ vite.config.ts
@@ -113,8 +113,8 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 5173,
-    allowedHosts: ['drumjot.kumo.dev'],
+    port: 5174,
+    allowedHosts: ['drumjot.kumo.dev', 'drumjot-dev.kumo.dev'],
   },
   preview: {
     host: true,
